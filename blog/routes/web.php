43b5cd0262c0f462db9ebe7a0321e4bda1ae60a0
Merike11 <merike.toose@ametikool.ee>
2020-04-16 23:00:50 +0300

    Muutsin login default value

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('articles.create');
 });
 Route::resource('articles', 'ArticleController');
 Route::resource('comments', 'CommentController');
